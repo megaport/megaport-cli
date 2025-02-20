3949fd74fe568282bbdb8a627b089b6c12820877
MegaportPhilipBrowne <philip.browne@megaport.com>
2025-02-20 09:07:13 -0500

    fix: use auth as a json file locally

--- internal/cmd/mp1.go
+++ internal/cmd/mp1.go
@@ -2,27 +2,61 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	megaport "github.com/megaport/megaportgo"
 	"github.com/spf13/cobra"
 )
 
 var (
-	accessKey string
-	secretKey string
+	accessKey  string
+	secretKey  string
+	configFile = filepath.Join(os.Getenv("HOME"), ".megaport-cli-config.json")
 )
+
+type Config struct {
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
+}
+
+func saveConfig(config Config) error {
+	file, err := os.Create(configFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(config)
+}
+
+func loadConfig() (Config, error) {
+	var config Config
+	file, err := os.Open(configFile)
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	return config, err
+}
 
 func Login(ctx context.Context) (*megaport.Client, error) {
 	httpClient := &http.Client{}
 
-	if accessKey == "" || secretKey == "" {
+	config, err := loadConfig()
+	if err != nil {
 		fmt.Println("Please provide access key and secret key using the configure command")
 		return nil, fmt.Errorf("access key and secret key are required")
 	}
 
-	megaportClient, err := megaport.New(httpClient, megaport.WithCredentials(accessKey, secretKey))
+	megaportClient, err := megaport.New(httpClient, megaport.WithCredentials(config.AccessKey, config.SecretKey))
 	if err != nil {
 		return nil, err
 	}
@@ -39,6 +73,17 @@ var configureCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		accessKey, _ = cmd.Flags().GetString("access-key")
 		secretKey, _ = cmd.Flags().GetString("secret-key")
+
+		config := Config{
+			AccessKey: accessKey,
+			SecretKey: secretKey,
+		}
+
+		if err := saveConfig(config); err != nil {
+			fmt.Println("Error saving configuration:", err)
+			return
+		}
+
 		fmt.Println("Configuration saved")
 	},
 }
