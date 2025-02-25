5ef74bbeacf561264f08cefe882c1b4ab0dabe11
MegaportPhilipBrowne <philip.browne@megaport.com>
2025-02-25 12:32:44 -0500

    feat: support command completion

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -24,6 +24,48 @@ This CLI supports the following features:
 `,
 }
 
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
+	Long: `To load completions:
+
+Bash:
+  $ source <(megaport completion bash)
+
+  # To load completions for each session, execute once:
+  # Linux:
+  $ megaport completion bash > /etc/bash_completion.d/megaport
+  # macOS:
+  $ megaport completion bash > /usr/local/etc/bash_completion.d/megaport
+
+Zsh:
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it.  You can execute the following once:
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+  # To load completions for each session, execute once:
+  $ megaport completion zsh > "${fpath[1]}/_megaport"
+
+Fish:
+  $ megaport completion fish > ~/.config/fish/completions/megaport.fish
+`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.MatchAll(cobra.ExactArgs(1)),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+		}
+	},
+}
+
 const (
 	formatTable = "table"
 	formatJSON  = "json"
@@ -61,6 +103,8 @@ func init() {
 			outputFormat, strings.Join(validFormats, ", "))
 	}
 	rootCmd.PersistentFlags().StringVarP(&env, "env", "e", "production", "Environment to use (production, staging, development)")
+	rootCmd.PersistentFlags().SetAnnotation("output", cobra.BashCompCustom, validFormats)
+	rootCmd.AddCommand(completionCmd)
 }
 
 func initConfig() {
