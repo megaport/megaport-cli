63bc5523cbae45d980a5be438885af9ba9dcf311
MegaportPhilipBrowne <philip.browne@megaport.com>
2026-04-17 06:36:42 -0700

    test: use require and concurrent drain in native prompt test

--- internal/utils/prompts_native_test.go
+++ internal/utils/prompts_native_test.go
@@ -4,10 +4,12 @@
 package utils
 
 import (
+	"io"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNativeInitAssignsPasswordPromptFn(t *testing.T) {
@@ -21,12 +23,23 @@ func TestNativePasswordPrompt_NonTTYReturnsError(t *testing.T) {
 	defer func() { os.Stdin, os.Stdout = oldStdin, oldStdout }()
 
 	inR, inW, err := os.Pipe()
-	assert.NoError(t, err)
+	require.NoError(t, err)
+	defer inR.Close()
+	defer inW.Close()
+
 	outR, outW, err := os.Pipe()
-	assert.NoError(t, err)
+	require.NoError(t, err)
+	defer outR.Close()
+
 	os.Stdin = inR
 	os.Stdout = outW
 	_ = inW.Close()
+
+	done := make(chan struct{})
+	go func() {
+		_, _ = io.Copy(io.Discard, outR)
+		close(done)
+	}()
 
 	for _, noColor := range []bool{true, false} {
 		pw, err := nativePasswordPrompt("Enter password:", noColor)
@@ -35,7 +48,5 @@ func TestNativePasswordPrompt_NonTTYReturnsError(t *testing.T) {
 	}
 
 	_ = outW.Close()
-	_, _ = outR.Read(make([]byte, 1024))
-	_ = inR.Close()
-	_ = outR.Close()
+	<-done
 }
